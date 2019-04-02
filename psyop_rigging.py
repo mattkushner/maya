af3d9fc3ad665aafc56269055e19bd49fd8dc05f
mattkushner <45632328+mattkushner@users.noreply.github.com>
2019-04-02 17:17:20 -0400

    Fixed function

--- psyop_rigging.py
+++ psyop_rigging.py
@@ -9,9 +9,8 @@ def update_ctrl_set():
   mc.sets(ctrls, n=ctrlSet) 
 
 
-
 def transfer_weights(source_shape, dest_shape):
     #for new models, pass both and skin to same joints and transfer weights
     joints = mc.skinCluster(source_shape, influence=True, query=True)
     mc.skinCluster(dest_shape, toSelectedBones=True, bindMethod=0, normalizeWeights=1, weightDistribution=0, maximumInfluences=5, obeyMaxInfluences=True, dropoffRate=4, removeUnusedInfluence=False)
-    mc.copySkinWeights(sourceSkin=source_shape, destinationSkin=dest_shape, noMirror=True, surfaceAssociation="closestPoint", influenceAssociation="label", influenceAssociation="oneToOne". influenceAssociation="closestJoint")
+    mc.copySkinWeights(sourceSkin=source_shape, destinationSkin=dest_shape, noMirror=True, surfaceAssociation="closestPoint", influenceAssociation=("label", "oneToOne", "closestJoint"))
