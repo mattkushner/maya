671fbdf473708ed4049ae701147eb29a910f5a7c
mattkushner <45632328+mattkushner@users.noreply.github.com>
2019-04-02 18:27:45 -0400

    Update psyop_rigging.py

--- psyop_rigging.py
+++ psyop_rigging.py
@@ -1,16 +1,30 @@
 import maya.cmds as mc
 
 def update_ctrl_set():
-  #takes all ctrls in scene and regenerates ctrl set from them
-  ctrls=mc.ls("*_Ctrl")
-  ctrlSet="ctrlSet"
-  if mc.objExists(ctrlSet):
-      mc.delete(ctrlSet)
-  mc.sets(ctrls, n=ctrlSet) 
+    #takes all ctrls in scene and regenerates ctrl set from them
+    ctrls=mc.ls("*_Ctrl")
+    ctrlSet="ctrlSet"
+    if mc.objExists(ctrlSet):
+        mc.delete(ctrlSet)
+    mc.sets(ctrls, n=ctrlSet) 
 
 
-def transfer_weights(source_shape, dest_shape):
+def transfer_weights(shape_dict):
     #for new models, pass both and skin to same joints and transfer weights
-    joints = mc.skinCluster(source_shape, influence=True, query=True)
-    mc.skinCluster(dest_shape, toSelectedBones=True, bindMethod=0, normalizeWeights=1, weightDistribution=0, maximumInfluences=5, obeyMaxInfluences=True, dropoffRate=4, removeUnusedInfluence=False)
-    mc.copySkinWeights(sourceSkin=source_shape, destinationSkin=dest_shape, noMirror=True, surfaceAssociation="closestPoint", influenceAssociation=("label", "oneToOne", "closestJoint"))
+    joints = mc.skinCluster(shape_dict['source']['shape'], influence=True, query=True)
+    shape_dict['source']['cluster'] =mel.eval("findRelatedSkinCluster "+shape_dict['source']['shape']+";")
+    shape_dict['dest']['cluster'] = mc.skinCluster(joints+[shape_dict['dest']['transform']], toSelectedBones=True, bindMethod=0, normalizeWeights=1, weightDistribution=0, maximumInfluences=5, obeyMaxInfluences=True, dropoffRate=4, removeUnusedInfluence=False)[0]
+    mc.copySkinWeights(sourceSkin=shape_dict['source']['cluster'], destinationSkin=shape_dict['dest']['cluster'], noMirror=True, surfaceAssociation="closestPoint", influenceAssociation=("label", "oneToOne", "closestJoint"))
+    print('Weights transferred from '+shape_dict['source']['transform']+' to '+shape_dict['dest']['transform']+'.')
+
+def transfer_selected():
+    shape_dict = {'source':{},'dest':{}}
+    objs = ['source', 'dest']
+    selected = mc.ls(selection=True, long=True)
+    if len(selected) == 2:
+        for i in range(len(objs)):
+            shape_dict[objs[i]]['transform'] = selected[i]
+            shape_dict[objs[i]]['shape'] = mc.listRelatives(selected[i], children=True, path=True)[0]
+        transfer_weights(shape_dict)
+    else:
+        print("Please select exactly two meshes for transfer.")
