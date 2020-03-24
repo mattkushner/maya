7819a087b21e552b2e172b0c87ba89ef1b4d2fb6
mattkushner <45632328+mattkushner@users.noreply.github.com>
2020-03-24 16:53:55 -0400

    Update rigging.py

--- rigging.py
+++ rigging.py
@@ -87,32 +87,44 @@ def pole_vector(y=45, leg='l_b'):
     
 def toe_group_setup(toe_name='l_b_index'):
     """Function to create single plane iks, parent into hierarchy and set pivots so they can be controlled"""
-    jnts_dict = {'toe': {'name': '', 'translates': [0,0,0], 'child': 'claw', 'grp': ''},
-                'ball': {'name': '', 'translates': [0,0,0], 'child': 'toe', 'grp': ''},
-                'ankle': {'name': '', 'translates': [0,0,0], 'child': 'ball', 'grp': ''}}
+    jnts_dict = {'end': {'name': '', 'translates': [0,0,0], 'child': '', 'grp': ''},
+                 'claw': {'name': '', 'translates': [0,0,0], 'child': 'end', 'grp': ''},
+                 'toe': {'name': '', 'translates': [0,0,0], 'child': 'claw', 'grp': ''},
+                 'ball': {'name': '', 'translates': [0,0,0], 'child': 'toe', 'grp': ''},
+                 'ankle': {'name': '', 'translates': [0,0,0], 'child': 'ball', 'grp': ''}}
     for name, jnt_dict in  jnts_dict.iteritems():
         jnt = '{T}_{N}_jnt'.format(T=toe_name, N=name)
         child = '{T}_{C}_jnt'.format(T=toe_name, C=jnt_dict['child'])
         jnts_dict[name]['name'] = jnt
         jnts_dict[name]['translates'] = mc.xform(jnt, query=True, worldSpace=True, translation=True)
         # create ik between jnt & child
-        ik = mc.ikHandle(startJoint=jnt, endEffector=child, solver='ikSCsolver')
-        ik_name = '{T}_{N}_ik'.format(T=toe_name, N=name)
-        grp_name = '{I}_grp'.format(I=ik_name).replace('_ik', 'Rotate')
-        mc.rename(ik[0], ik_name)
-        mc.group(ik_name, name=grp_name)
-        jnts_dict[name]['grp'] = grp_name
-        mc.move(jnts_dict[name]['translates'][0],jnts_dict[name]['translates'][1],jnts_dict[name]['translates'][2],'{G}.rotatePivot'.format(G=grp_name),'{G}.scalePivot'.format(G=grp_name), rpr=True)
-        #connect ctrls to rotate grps
-        ctrl = jnt.replace('jnt', 'ctrl')
-        for attr in ['x', 'y', 'z']:
-            mc.connectAttr('{C}.rotate{A}'.format(C=ctrl, A=attr.capitalize()), '{G}.rotate{A}'.format(G=grp_name, A=attr.capitalize()))
+        if name != 'end':
+            ik = mc.ikHandle(startJoint=jnt, endEffector=child, solver='ikSCsolver')
+            ik_name = '{T}_{N}_ik'.format(T=toe_name, N=name)
+            grp_name = '{I}_grp'.format(I=ik_name).replace('_ik', 'Rotate')
+            mc.rename(ik[0], ik_name)
+            mc.group(ik_name, name=grp_name)
+            jnts_dict[name]['grp'] = grp_name
+            mc.move(jnts_dict[name]['translates'][0],jnts_dict[name]['translates'][1],jnts_dict[name]['translates'][2],'{G}.rotatePivot'.format(G=grp_name),'{G}.scalePivot'.format(G=grp_name), rpr=True)
+            #connect ctrls to rotate grps
+            ctrl = jnt.replace('jnt', 'ctrl')
+            for attr in ['x', 'y', 'z']:
+                mc.connectAttr('{C}.rotate{A}'.format(C=ctrl, A=attr.capitalize()), '{G}.rotate{A}'.format(G=grp_name, A=attr.capitalize()))
     # nest groups and add parents
+    mc.parent(jnts_dict['claw']['grp'], jnts_dict['toe']['grp'])
     mc.parent(jnts_dict['toe']['grp'], jnts_dict['ball']['grp'])
     mc.parent(jnts_dict['ball']['grp'], jnts_dict['ankle']['grp'])
     grp = mc.group(jnts_dict['ankle']['grp'], name='{T}_heelRotate_grp'.format(T=toe_name))
     mc.move(jnts_dict['ankle']['translates'][0],0,jnts_dict['ankle']['translates'][2],'{G}.rotatePivot'.format(G=grp),'{G}.scalePivot'.format(G=grp), rpr=True)
     grp = mc.group(grp, name='{T}_heelPivot_grp'.format(T=toe_name))
     mc.move(jnts_dict['ankle']['translates'][0],0,jnts_dict['ankle']['translates'][2]+.01,'{G}.rotatePivot'.format(G=grp),'{G}.scalePivot'.format(G=grp), rpr=True)
-    grp = mc.group(grp, name='{T}_toeStand_grp'.format(T=toe_name))
+    grp = mc.group(grp, name='{T}_ballPivot_grp'.format(T=toe_name))
     mc.move(jnts_dict['toe']['translates'][0],jnts_dict['toe']['translates'][1],jnts_dict['toe']['translates'][2],'{G}.rotatePivot'.format(G=grp),'{G}.scalePivot'.format(G=grp), rpr=True)
+    grp = mc.group(grp, name='{T}_toePivot_grp'.format(T=toe_name))
+    mc.move(jnts_dict['claw']['translates'][0],jnts_dict['claw']['translates'][1],jnts_dict['claw']['translates'][2],'{G}.rotatePivot'.format(G=grp),'{G}.scalePivot'.format(G=grp), rpr=True)    
+    grp = mc.group(grp, name='{T}_toeStand_grp'.format(T=toe_name))
+    mc.move(jnts_dict['claw']['translates'][0],jnts_dict['claw']['translates'][1],jnts_dict['claw']['translates'][2],'{G}.rotatePivot'.format(G=grp),'{G}.scalePivot'.format(G=grp), rpr=True)
+    grp = mc.group(grp, name='{T}_clawPivot_grp'.format(T=toe_name))
+    mc.move(jnts_dict['end']['translates'][0],jnts_dict['end']['translates'][1],jnts_dict['end']['translates'][2],'{G}.rotatePivot'.format(G=grp),'{G}.scalePivot'.format(G=grp), rpr=True)    
+    grp = mc.group(grp, name='{T}_clawStand_grp'.format(T=toe_name))
+    mc.move(jnts_dict['end']['translates'][0],jnts_dict['end']['translates'][1],jnts_dict['end']['translates'][2],'{G}.rotatePivot'.format(G=grp),'{G}.scalePivot'.format(G=grp), rpr=True)
